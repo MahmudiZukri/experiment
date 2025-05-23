a233e430fdfa6ba701adf58ae614b1918fcb1c47
MahmudiZukri <mahmudizukri@gmail.com>
2025-05-23 13:52:21 +0700

    webview_flutter routing work properly

--- lib/main.dart
+++ lib/main.dart
@@ -2,21 +2,53 @@ import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 void main() {
-  runApp(const MyApp(
-    treatmentId: '19',
-  ));
+  runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  final String treatmentId; // data you want to pass
-
-  const MyApp({super.key, required this.treatmentId});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: FirstPage(),
+    );
+  }
 }
 
-class _MyAppState extends State<MyApp> {
+class FirstPage extends StatelessWidget {
+  const FirstPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('First Page')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const WebViewPage(treatmentId: '19'),
+              ),
+            );
+          },
+          child: const Text('Open WebView'),
+        ),
+      ),
+    );
+  }
+}
+
+class WebViewPage extends StatefulWidget {
+  final String treatmentId;
+  const WebViewPage({super.key, required this.treatmentId});
+
+  @override
+  State<WebViewPage> createState() => _WebViewPageState();
+}
+
+class _WebViewPageState extends State<WebViewPage> {
   late WebViewController _controller;
 
   @override
@@ -24,29 +56,57 @@ class _MyAppState extends State<MyApp> {
     super.initState();
     _controller = WebViewController()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..loadRequest(Uri.parse("https://172.16.5.38:3004/portal/treatment"))
+      // ..setNavigationDelegate(
+      //   NavigationDelegate(
+      //     onPageFinished: (String url) {
+      //       _controller.runJavaScript(
+      //           'window.receiveFromFlutter("${widget.treatmentId}");');
+      //     },
+      //   ),
+      // )
       ..addJavaScriptChannel(
         'FlutterChannel',
-        onMessageReceived: (JavaScriptMessage message) {
-          debugPrint("Message from WebView: ${message.message}");
+        onMessageReceived: (JavaScriptMessage message) async {
+          debugPrint("Received from WebView: ${message.message}");
+          if (message.message == "goBack") {
+            if (await _controller.canGoBack()) {
+              debugPrint('asdasd 777');
+              _controller.goBack();
+            } else {
+              if (mounted) Navigator.pop(context);
+            }
+          } else {
+            // Handle other messages, e.g., note submission
+            debugPrint("Message: ${message.message} asdasd");
+          }
         },
       )
-      ..setNavigationDelegate(NavigationDelegate(
-        onPageFinished: (String url) {
-          // Once the page is loaded, send data
-          _controller.runJavaScript(
-            'window.receiveFromFlutter("${widget.treatmentId}");',
-          );
-        },
-      ));
+      ..loadRequest(
+        Uri.parse('http://172.16.5.38:3004/portal/treatment'),
+      );
+  }
+
+  Future<bool> _handleBackPressed() async {
+    // This will call JS window.goBackInApp() from Flutter
+    await _controller.runJavaScriptReturningResult('window.goBackInApp()');
+
+    debugPrint('asdasd 5555');
+    return false; // Prevent Flutter from popping until JS says so
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Treatment WebView')),
-        body: WebViewWidget(controller: _controller),
+    return PopScope(
+      canPop: false,
+      onPopInvoked: (didPop) async {
+        debugPrint('asdasd');
+        if (didPop == false) {
+          debugPrint('asdasd 444');
+          _handleBackPressed();
+        }
+      },
+      child: Scaffold(
+        body: SafeArea(child: WebViewWidget(controller: _controller)),
       ),
     );
   }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,9 +13,7 @@ import 'package:experiment/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp(
-      treatmentId: '1',
-    ));
+    // await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
